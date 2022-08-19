ebf255a01a0a41187b24acb54e9818ced97d1b40
preet-design <mk5036683@gmail.com>
2022-08-19 21:25:22 +0530

    login done

--- module5/foodApp/server.js
+++ module5/foodApp/server.js
@@ -23,9 +23,37 @@ app.post("/signup", async function(req,res){
         console.log(data);
         res.json({
             message:"data recieved",
+            //data : data --> for ahow full data in db
         })}
     catch(err){
         res.send(err.message)
+    }
+})
+
+app.post("/login",async function(req,res){
+    try{
+        let data = req.body;
+        console.log(data);
+        let {email, password}=data;
+        if(email && password){
+            let user = await userModel.findOne({email:email});
+                if(user){
+                    if(user.password == password){
+                        res.send("User logged in");
+                    }else{
+                        res.send("email and password does not match");
+                    }
+                }else{
+                    res.send("User with this email does not exist.Kindly sign up");
+                }
+        }else{
+            res.send("Kindly Enter Correct Email and Password ");
+        }
+        // console.log(user);
+        // res.send("data recieved");
+    }catch(err){
+            console.log(err.message);
+        
     }
 })
 
